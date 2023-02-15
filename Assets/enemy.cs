dca3a3635649011c4903bee49ebd9f3e5f8566db
Doriwis <arnodorianmt03@gmail.com>
2023-02-15 21:00:10 +0100

    first day

---
+++ Assets/enemy.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    [SerializeField] Transform patrolPath;
+    [SerializeField] float velocidad;
+    
+
+    Transform[] destins;
+    int indicesDestiny;
+    Vector3 destinoactual;
+    void Start()
+    {
+        destins = new Transform[patrolPath.childCount];
+        for (int i = 0; i < destins.Length; i++)
+        {
+            destins[i] = patrolPath.GetChild(i);
+        }
+
+        destinoactual = destins[indicesDestiny].position;
+
+
+
+        StartCoroutine(MoverAPuntoYEsperar());
+    }
+
+
+    void Update()
+    {
+        
+    }
+    
+    IEnumerator MoverAPuntoYEsperar()
+    {
+        while (true)
+        {
+
+          while (transform.position!=destinoactual)
+          {
+            transform.position = Vector3.MoveTowards(transform.position, destinoactual, velocidad * Time.deltaTime);
+            yield return null;
+          }
+
+            Contador();
+
+          yield return new WaitForSeconds(1);
+        }
+      
+
+        
+    }
+    void Contador()
+    {
+        indicesDestiny++;
+        if (indicesDestiny >= destins.Length)
+        {
+            indicesDestiny = 0;
+        }
+        destinoactual = destins[indicesDestiny].position;
+
+        float diferencia = transform.position.x - destinoactual.x;
+
+        transform.rotation = diferencia > 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.Euler(0, 0, 0);
+        
+        
+    }
+
+}
--- Assets/player.cs
+++ Assets/player.cs
@@ -4,34 +4,73 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    Rigidbody rb;
+    Rigidbody2D rb;
+    float h;
+
+    [Header("Mov")]
+    [SerializeField]float velocityMovimiento;
     [SerializeField] float velocidadMaxima;
+    Vector3 ajuste;
+
+    [Header("salto")]
     [SerializeField] float Force;
     [SerializeField] float ForecJumop;
-    float h;
-    float velocityMovimiento;
-        
+
+
+    [Header("bolasuelo")]
+    [SerializeField] Transform pies;
+    [SerializeField] float radio;
+    [SerializeField] LayerMask Suelo;
+    [SerializeField] int saltosMax;
+    int salirosDisponibles;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>()
-            ;
+        rb = GetComponent<Rigidbody2D>();
+        salirosDisponibles = saltosMax;
     }
 
     // Update is called once per frame
     void Update()
     {
          h = Input.GetAxisRaw("Horizontal");
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)&& salirosDisponibles>0)
         {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.AddForce(new Vector3(0, 1, 0) * ForecJumop,ForceMode2D.Impulse);
+            salirosDisponibles--;
 
         }
-        Vector3.ClampMagnitude(rb.velocity, velocidadMaxima);
-    }
-    void a()
-    {
-        rb.velocity = new Vector3(h* velocityMovimiento, rb.velocity.y);
+        ItsGrounded();
+
+        
     }
     
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(h * velocityMovimiento, rb.velocity.y);
+        ajuste= Vector3.ClampMagnitude(rb.velocity, velocidadMaxima);
+        rb.velocity = new Vector3 (ajuste.x,rb.velocity.y);
+    }
+
+    bool  ItsGrounded()
+    {
+       Collider2D coll= Physics2D.OverlapCircle(pies.position, radio, Suelo);
+        if (coll!=null)
+        {
+            if (rb.velocity.y<=0)
+            {
+                salirosDisponibles = saltosMax;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(pies.position, radio);
+    }
+
 }
